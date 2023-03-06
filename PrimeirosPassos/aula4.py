5fc0f3235a1e2d9e8f047ec762f0dd8f20640aa3
Luana Landulpho Martini <luana.landmartini@gmail.com>
2023-03-06 18:03:13 -0300

    feitos alguns exercícios da aula 4

---
+++ PrimeirosPassos/aula4.py
@@ -0,0 +1,20 @@
+print('Olá, Mundo!')
+
+print(7+4)
+
+print('7'+'4')
+
+print('Olá' , 5)
+
+nome = 'Luana'
+idade = 19
+peso = 40.5
+
+print(nome,idade,peso)
+
+nome2 = input('Qual o seu nome? ')
+idade2 = input('Qual sua idade? ')
+peso2 = input('Qual o seu peso? ')
+print(nome2,idade2,peso2)
+
+
---
+++ PrimeirosPassos/aula4ex2.py
@@ -0,0 +1,6 @@
+dia = input('Qual dia você nasceu? ')
+mes = input('De qual mês? ')
+ano = input('De qual ano? ')
+
+print('Ah! Então você nasceu dia '+dia+'/'+mes+'/'+ano+', certo?')
+
