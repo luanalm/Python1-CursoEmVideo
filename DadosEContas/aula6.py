b58005299ab53b9bafcc1c0d6c3480759cb8a542
Luana Landulpho Martini <luana.landmartini@gmail.com>
2023-03-06 19:59:50 -0300

    aula 6

---
+++ DadosEContas/aula6.py
@@ -0,0 +1,18 @@
+n = bool(input('Digite um valor: '))
+print(n)
+
+m = input('Digite algo: ')
+print(m.isnumeric())
+
+b = input('Digite algo: ')
+print(b.isalpha())
+
+v = input('Digite algo: ')
+print(v.isalnum())
+
+c = input('Digite algo: ')
+print(c.isupper())
+
+p = input('Digite algo: ')
+print(p.isprintable())
+
---
+++ DadosEContas/aula6ex1.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+
+#print('A soma entre ', n1, ' e ', n2, ', vale ', s)87
+print('A soma entre {0} e {1}, vale {2}'.format(n1, n2, s))
+45
