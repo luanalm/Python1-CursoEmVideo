b84ea4e78b321eb7d99dfa7698a9106055c751a1
Luana Landulpho Martini <luana.landmartini@gmail.com>
2023-03-07 02:04:03 -0300

    mudei a média para float

--- DadosEContas/aula7ex4.py
+++ DadosEContas/aula7ex4.py
@@ -2,9 +2,9 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
 print('Vamos calcular a média entre suas duas notas')
-x = int(input('Primeira nota: '))
-y = int(input('Segunda nota: '))
+x = float(input('Primeira nota: '))
+y = float(input('Segunda nota: '))
 
 m = (x + y)/2
 
-print('A média entre suas notas é {}'.format(m))
+print('A média entre suas notas é {:.2f}'.format(m))
