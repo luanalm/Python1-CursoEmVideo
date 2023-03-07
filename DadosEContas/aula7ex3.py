e151076ae0c152fbe28320020091cede26bdd506
Luana Landulpho Martini <luana.landmartini@gmail.com>
2023-03-07 02:30:26 -0300

    só formatação

--- DadosEContas/aula7ex3.py
+++ DadosEContas/aula7ex3.py
@@ -5,6 +5,6 @@ x = int(input('Escolha um número: '))
 
 d = x * 2
 t = x * 3
-r = x ** 0.5
+r = x ** (1/2)
 
 print('O dobro do número escolhido é {}, o seu triplo é {} e a sua raíz quadrada é {}'.format(d, t, r))
--- DadosEContas/aula7ex7.py
+++ DadosEContas/aula7ex7.py
@@ -1,7 +1,7 @@
 # Exercício 10
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere US$1,00 = R$3,27
 
-x = int(input('Quanto dinheiro você tem na carteira? '))
+x = int(input('Quanto dinheiro você tem na carteira? R$'))
 
 dol = x / 3.27
 
