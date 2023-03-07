fb27cce9acda2fd1c4982bc32ff3e6e546a474fa
Luana Landulpho Martini <luana.landmartini@gmail.com>
2023-03-07 12:12:41 -0300

    exercício 14 feito

---
+++ DadosEContas/exercício14.py
@@ -0,0 +1,16 @@
+# Conversor de Temperaturas
+
+print('Você deseja converter qual temperatura?')
+x = int(input('Digite 0 para Celsius ou digite 1 para Fahrenheit: '))
+
+if x == 0 :
+    c = float(input('A temperatura em graus Celsius: '))
+    f = (c*1.8)+32
+    print('A temperatura em Fahrenheit é {:.1f}'.format(f))
+elif x == 1:
+    f = float(input('A temperatura em graus Fahrenheit: '))
+    c = (f-32)/1.8
+    print('A temperatura em Celsius é {:.1f}'.format(c))
+else:
+    print('O número escrito é inválido, tente novamente!')
+    
