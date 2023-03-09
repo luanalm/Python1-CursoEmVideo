2d11e1141bf9ffb47e6f44c3de4f342efc35e7cd
Luana Landulpho Martini <luana.landmartini@gmail.com>
2023-03-09 02:18:19 -0300

    simplificado

--- UsandoModulos/aula8ex3.py
+++ UsandoModulos/aula8ex3.py
@@ -1,12 +1,12 @@
 # Exercício 18
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 
-import math
+from math import radians, sin, cos, tan
 
 ang = float(input('O valor de um ângulo: '))
 
-s = math.sin(math.radians(ang))
-c = math.cos(math.radians(ang))
-t = math.tan(math.radians(ang))
+s = sin(radians(ang))
+c = cos(radians(ang))
+t = tan(radians(ang))
 
 print('O ângulo {:.2f}, possui o seno {:.2f}, o cosseno {:.2f} e a tangente {:.2f}'.format(ang, s, c, t))
